26c675375ba5099699a7aa53f52c48c9d2142afa
Tejera Cordero, Ruben <ruben.tejera@external.wolterskluwer.com>
2025-09-03 11:37:42 +0100

    Aplicar gitignore

--- RomanNumeralsKata/RomanConverter.cs
+++ RomanNumeralsKata/RomanConverter.cs
@@ -5,6 +5,20 @@ public class RomanConverter
     public string ToRoman(int number)
     {
         // lógica para convertir número a romano
-        return "I";
+        
+        if (number == 1)
+        {
+          return "I";  
+        }
+        if (number == 2)
+        {
+          return "II";  
+        }
+        if (number == 3)
+        {
+          return "III";  
+        }
+        return "";
+        
     }
 }
--- Tests/RomanCoverterTests.cs
+++ Tests/RomanCoverterTests.cs
@@ -11,4 +11,19 @@ public class RomanConverterTests
         var converter = new RomanConverter();
         Assert.AreEqual("I", converter.ToRoman(1));
     }
+    
+    [Test]
+    public void TwoIsII()
+    {
+        var converter = new RomanConverter();
+        Assert.AreEqual("II", converter.ToRoman(2));
+    }
+    
+    [Test]
+    public void ThreeIsIII()
+    {
+        var converter = new RomanConverter();
+        Assert.AreEqual("III", converter.ToRoman(3));
+    }
+    
 }
