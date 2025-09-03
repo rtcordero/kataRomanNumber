96e451e6aeb5e8d76d7f535e84c4b88a759e9dd1
Tejera Cordero, Ruben <ruben.tejera@external.wolterskluwer.com>
2025-09-03 13:22:25 +0100

    refactor Roman numeral conversion to use a while loop for improved logic

--- RomanNumeralsKata/RomanConverter.cs
+++ RomanNumeralsKata/RomanConverter.cs
@@ -5,25 +5,30 @@ public class RomanConverter
     public string ToRoman(int number)
     {
         // lógica para convertir número a romano
-        
+
         var result = "";
 
-        for (int i = 0; i < number; i++)
+        while (number > 0)
         {
-            if(number == 5) {
+            if (number == 5)
+            {
                 result += "V";
-                break;
+                number -= 5;
+                continue;
             }
-            if(number == 4) {
-                result += "IV";
-                break;
-            }
-            result += "I";
 
+            if (number == 4)
+            {
+                result += "IV";
+                number -= 4;
+                continue;
+            }
+
+            result += "I";
+            number -= 1;
         }
-            
+
 
         return result;
-        
     }
 }
