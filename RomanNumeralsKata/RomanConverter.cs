34f7c2e76d3a7f75c4a7cb9c6a4d689f7eb71c9d
Tejera Cordero, Ruben <ruben.tejera@external.wolterskluwer.com>
2025-09-03 14:42:21 +0100

    refactor Roman numeral conversion to use foreach loop for improved clarity and maintainability

--- RomanNumeralsKata/RomanConverter.cs
+++ RomanNumeralsKata/RomanConverter.cs
@@ -29,60 +29,14 @@ public class RomanConverter
 
         while (number > 0)
         {
-            switch (number)
+            foreach (var romanMap in RomanMap)
             {
-                case >= 1000:
-                    result.Append(RomanMap[1000]);
-                    number -= 1000;
-                    continue;
-                case >= 900: //Sustracción
-                    result.Append(RomanMap[900]);
-                    number -= 900;
-                    continue;
-                case >= 500:
-                    result.Append(RomanMap[500]);
-                    number -= 500;
-                    continue;
-                case >= 400: //Sustracción
-                    result.Append(RomanMap[400]);
-                    number -= 400;
-                    continue;
-                case >= 100:
-                    result.Append(RomanMap[100]);
-                    number -= 100;
-                    continue;
-                case >= 90: //Sustracción
-                    result.Append(RomanMap[90]);
-                    number -= 90;
-                    continue;
-                case >= 50:
-                    result.Append(RomanMap[50]);
-                    number -= 50;
-                    continue;
-                case >= 40: //Sustracción
-                    result.Append(RomanMap[40]);
-                    number -= 40;
-                    continue;
-                case >= 10:
-                    result.Append(RomanMap[10]);
-                    number -= 10;
-                    continue;
-                case >= 9: //Sustracción
-                    result.Append(RomanMap[9]);
-                    number -= 9;
-                    continue;
-                case >= 5:
-                    result.Append(RomanMap[5]);
-                    number -= 5;
-                    continue;
-                case >= 4: //Sustracción
-                    result.Append(RomanMap[4]);
-                    number -= 4;
-                    continue;
-                default:
-                    result.Append(RomanMap[1]);
-                    number -= 1;
+                if (number >= romanMap.Key)
+                {
+                    result.Append(romanMap.Value);
+                    number -= romanMap.Key;
                     break;
+                }
             }
         }
 
