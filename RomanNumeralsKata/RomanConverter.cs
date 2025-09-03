ed816355afad680c617bab1312c1cd44e3e16453
Tejera Cordero, Ruben <ruben.tejera@external.wolterskluwer.com>
2025-09-03 14:36:17 +0100

    refactor Roman numeral conversion to use Dictionary for mapping values and improve readability

--- RomanNumeralsKata/RomanConverter.cs
+++ RomanNumeralsKata/RomanConverter.cs
@@ -4,21 +4,21 @@ namespace RomanNumeralsKata;
 
 public class RomanConverter
 {
-    private static readonly (int Value, string Symbol)[] RomanMap =
+    private static readonly Dictionary<int, string> RomanMap = new()
     {
-        (1000, "M"),
-        (900, "CM"),
-        (500, "D"),
-        (400, "CD"),
-        (100, "C"),
-        (90, "XC"),
-        (50, "L"),
-        (40, "XL"),
-        (10, "X"),
-        (9, "IX"),
-        (5, "V"),
-        (4, "IV"),
-        (1, "I")
+        {1000, "M"},
+        {900,  "CM"},
+        {500,  "D"},
+        {400,  "CD"},
+        {100,  "C"},
+        {90,   "XC"},
+        {50,   "L"},
+        {40,   "XL"},
+        {10,   "X"},
+        {9,    "IX"},
+        {5,    "V"},
+        {4,    "IV"},
+        {1,    "I"}
     };
 
     public string ToRoman(int number)
@@ -32,55 +32,55 @@ public class RomanConverter
             switch (number)
             {
                 case >= 1000:
-                    result.Append('M');
+                    result.Append(RomanMap[1000]);
                     number -= 1000;
                     continue;
                 case >= 900: //Sustracción
-                    result.Append("CM");
+                    result.Append(RomanMap[900]);
                     number -= 900;
                     continue;
                 case >= 500:
-                    result.Append("D");
+                    result.Append(RomanMap[500]);
                     number -= 500;
                     continue;
                 case >= 400: //Sustracción
-                    result.Append("CD");
+                    result.Append(RomanMap[400]);
                     number -= 400;
                     continue;
                 case >= 100:
-                    result.Append("C");
+                    result.Append(RomanMap[100]);
                     number -= 100;
                     continue;
                 case >= 90: //Sustracción
-                    result.Append("XC");
+                    result.Append(RomanMap[90]);
                     number -= 90;
                     continue;
                 case >= 50:
-                    result.Append('L');
+                    result.Append(RomanMap[50]);
                     number -= 50;
                     continue;
                 case >= 40: //Sustracción
-                    result.Append("XL");
+                    result.Append(RomanMap[40]);
                     number -= 40;
                     continue;
                 case >= 10:
-                    result.Append('X');
+                    result.Append(RomanMap[10]);
                     number -= 10;
                     continue;
                 case >= 9: //Sustracción
-                    result.Append("IX");
+                    result.Append(RomanMap[9]);
                     number -= 9;
                     continue;
                 case >= 5:
-                    result.Append('V');
+                    result.Append(RomanMap[5]);
                     number -= 5;
                     continue;
                 case >= 4: //Sustracción
-                    result.Append("IV");
+                    result.Append(RomanMap[4]);
                     number -= 4;
                     continue;
                 default:
-                    result.Append('I');
+                    result.Append(RomanMap[1]);
                     number -= 1;
                     break;
             }
